8327241eb83da07c3defcadeb68f6daac4250386
Dhanush H V <dhanushhv75@gmail.com>
2025-02-01 02:06:30 +0530

    Removed extra space from license comment

--- logger/logger.h
+++ logger/logger.h
@@ -1,9 +1,7 @@
 /*
  *  ************************************************************************
- *  
  *  Copyright (c) 2025, Dhanush H V. All rights reserved.
  *  Licensed under the MIT License. See the LICENSE file for more details
- *  
  *  *************************************************************************
  */
 
